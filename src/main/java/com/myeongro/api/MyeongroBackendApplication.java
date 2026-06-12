603b8b225a29c511b9b4056587423c3a77231674
100gammmit <100minha@naver.com>
2026-06-12 16:09:10 +0900

    프로젝트 생성

---
+++ src/main/java/com/myeongro/api/MyeongroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myeongro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyeongroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyeongroBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/myeongro/api/domain/consent/entity/consent.java
@@ -0,0 +1,12 @@
+package com.myeongro.api.domain.consent.entity;
+
+import jakarta.persistence.Entity;
+
+/**
+ * consent
+ * <p></p>
+ * @author 100minha
+ */
+@Entity
+public class consent {
+}
